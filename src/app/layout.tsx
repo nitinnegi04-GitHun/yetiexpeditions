99e55d2ab6f62fe24ae44ff6082045b543ae35d2
Nitin Negi <nitinnegi@Nitins-MacBook-Air.local>
2026-07-14 16:30:59 +0530

    feat: add Google Analytics (GA4) via next/script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Public_Sans } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { client } from "@/sanity/client";
 import { SITE_SETTINGS_QUERY } from "@/sanity/queries/siteSettings";
@@ -152,6 +153,22 @@ export default async function RootLayout({
         />
       </head>
       <body className={`${publicSans.variable} font-body antialiased`}>
+        {process.env.NEXT_PUBLIC_GA_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+              `}
+            </Script>
+          </>
+        )}
         <CurrencyProvider>
           {children}
         </CurrencyProvider>
